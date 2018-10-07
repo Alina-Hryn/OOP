9d92d0c025489fe85c960b942bdc2258fd29cfaa
Alina-Hryn <43728667+Alina-Hryn@users.noreply.github.com>
2018-10-08 00:42:07 +0300

    lab5 task 7

--- 5lab7/5lab7/Source.cpp
+++ 5lab7/5lab7/Source.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 class Point {
 	char name;
-	int x;
-	int y;
+	float x;
+	float y;
 public:
 	void make() {
-		int a, b;
+		float a, b;
 		cout << "Point Name(only one letter): ";
 		cin >> name;
 		cout << "x: ";
@@ -20,28 +20,28 @@ public:
 		cout << name << "(" << x << ", " << y << ") " << endl;
 	}
 	void symmetricMappingOx() {
-		cout << "symmetric Mapping by Ox: " << endl;
+		//cout << "symmetric Mapping by Ox: " << endl;
 		y = -y;
-		show();
 	}
 	void symmetricMappingOy() {
-		cout << "symmetric Mapping by Oy:" << endl;
+		//cout << "symmetric Mapping by Oy:" << endl;
 		x = -x;
-		show();
 	}
-	void movePoint() {
-		
-		int a, b;
-		cout << "move point by Ox: " << endl;
-		cin >> a;
-		y += a;
-		cout << "move point by Oy: " << endl;
-		cin >> b;
-		x += b;
-		
+	void movePoint(float a, float b) {
+		x += a;
+		y += b;
+
 	}
 	char pname() {
 		char a = name;
+		return a;
+	}
+	float px() {
+		float a = x;
+		return a;
+	}
+	float py() {
+		float a = y;
 		return a;
 	}
 
@@ -52,10 +52,20 @@ class Line {
 	Point begin;
 	Point end;
 public:
-	void make() {
+	void makeNewLineWithNewPoints() {
+		cout << "  Making New Line" << endl;
 		cout << "Line : ";
 		begin.make();
 		end.make();
+		name[0] = begin.pname();
+		name[1] = end.pname();
+		name[2] = '\0';
+	}
+	void make(Point a, Point b ) {
+		cout << "  Making New Line " << endl;
+		cout << "Line : ";
+		begin = a;
+		end=b;
 		name[0] = begin.pname();
 		name[1] = end.pname();
 		name[2] = '\0';
@@ -68,10 +78,45 @@ public:
 		end.show();
 	}
 	void MoveLine() {
-		int a, b;
-
+		cout << "  Moving line" << endl;
+		float a, b;
+		cout << "move by Ox: " << endl;
+		cin >> a;
+		cout << "move by Oy: " << endl;
+		cin >> b;
+		begin.movePoint(a, b);
+		end.movePoint(a, b);
+	}
+	void symmetricMapping() {
+		int a;
+		cout << "  If you want to symmetrically reflect by Ox, press 1;" << endl;
+		cout << "  If you want to symmetrically reflect by Oy, press 2;" << endl;
+		cout << "  If you want to symmetrically reflect both by Ox and Oy, press 3;" << endl;
+		cin >> a;
+		if (a == 1) {
+			begin.symmetricMappingOx();
+			end.symmetricMappingOx();
+		}
+		if (a == 2) {
+			begin.symmetricMappingOy();
+			end.symmetricMappingOy();
+		}
+		if (a == 3) {
+			begin.symmetricMappingOx();
+			end.symmetricMappingOx();
+			begin.symmetricMappingOy();
+			end.symmetricMappingOy();
+		}
+	}
+	float length() {
+		float result;
+		result = ((end.px()-begin.px())^2+(end.py() - begin.py())^2) ^ (1 / 2);
 	}
 
+
+};
+
+class Parallelogram {
 
 };
 
@@ -81,7 +126,11 @@ int main() {
 	point.show();
 	point.symmetricMappingOx();*/
 	Line line;
-	line.make();
+	line.makeNewLineWithNewPoints();
+	line.show();
+	line.MoveLine();
+	line.show();
+	line.symmetricMapping();
 	line.show();
 	system("pause");
 	return 0;
